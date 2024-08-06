096dc1348e6a868e85db95900e31e4473385ccd6
varun1130 <143049548+varun1130@users.noreply.github.com>
2024-08-06 16:23:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Weather from './Components/Weather';
+
+function App() {
+  return (
+  <Weather/>
+  );
+}
+
+export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import "./weatherStyle.css"
+import axios from "axios"
+
+const Weather = () => {
+  //creating statehook
+  const[city ,setcity] = useState();  
+  const [wData ,setwData] = useState();
+
+  //calling this function as we type in the inputfield
+  const HandelData = (event) =>{
+    setcity(event.target.value);
+  }
+
+  const getdata = () =>{
+    fetchApi();
+  }
+  const fetchApi = async()=> {
+    try{
+      let Data = await axios.get();//put your api in get
+      
+      setwData(Data);
+      console.log(Data);
+    }
+    catch(error){
+      console.log("Error",error)
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="Ldata">Weather App</h1>
+      <div className="weather">
+        <input id="locationInput" type="text" placeholder="Enter City" value={city}  onChange={HandelData}/><br/>
+        <button id="searchButton" onClick={getdata}>Get Weather</button>
+        {wData &&<>
+          <h3 className="Ldata">{wData.data.name}</h3>
+          <h4 className="Ldata">Temp :{Math.round(wData.data.main.temp)}</h4>
+          <p className="Ldata">{wData.data.weather[0].description}</p>
+        </>}
+      </div>
+    </div>
+  )
+}
+
+export default Weather
